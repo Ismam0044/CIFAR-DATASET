2f0a6b37b979fb3e2dfad4a62284799722748b15
Md. Ismam Hossain Redwan <ismamhossain2000@gmail.com>
2026-07-02 02:09:28 +0600

    Delete cifar.py

--- cifar.py
+++
@@ -1,8 +0,0 @@
-
-import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
-import matplotlib.pyplot as plt
-import numpy as np
-
-(X_train, y_train), (X_test,y_test) = datasets.cifar10.load_data()
-X_train.shape
